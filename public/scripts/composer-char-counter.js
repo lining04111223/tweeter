df8523b9fdb53f236ba7411bff0da8bdb31c3ea4
Ning Li <lining04111223@gmail.com>
2022-10-12 03:19:15 +0000

    add character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("textarea").on('input', function() {
+  let remainNum = 140;
+  const currentCount = $(this).val().length;
+  remainNum  -= currentCount;
+  const counter = $(this).parent().siblings(".formbottom").children("output")[0];
+  counter.innerText = remainNum.toString();
+  if (remainNum < 0) {
+    $(counter).addClass("changeToRed");
+  }
+  })
+});
+
+
