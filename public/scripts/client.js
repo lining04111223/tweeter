d7e17c0d77d4eb6913253b234e1215d800617d6e
Ning Li <lining04111223@gmail.com>
2022-10-13 00:19:31 +0000

    fetch tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,30 +5,8 @@
  */
 // Test / driver code (temporary). Eventually will get this from the server.
 // Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
+$(document).ready(() => {
 
 const renderTweets = function(data) {
   for (const dataid of data) {
@@ -65,7 +43,7 @@ let $tweet = `
 return $tweet;
 }
 
-renderTweets(data);
+
 
 // grab the form
   const $form = $('.form-inline');
@@ -78,18 +56,33 @@ renderTweets(data);
   console.log(dataToSendToServer);
 
   // send the information to the server via a POST request
-  $.ajax({
+   $.ajax({
     url: '/tweets',
     method: 'POST',
     data: dataToSendToServer
-  })
-  .then((tweetdata) => {
+   })
+   .then((tweetdata) => {
     console.log("tweetdata",tweetdata);
+   })
+   .catch((err) => {
+    console.log("err",err);
+   });
+  });
+  
+
+  // make an AJAX request for all the tweets
+  $.ajax({
+    url: '/tweets',
+    method: 'GET'
+  })
+  .then((data) => {
+
+    renderTweets(data);    
+    console.log(data);
   })
   .catch((err) => {
-    console.log("err",err);
+    console.log(err);
   });
-
 
 
 
